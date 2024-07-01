41982c2ece2a6d38318803397c847d7e55d9e331
alex2000d <demarcoalessandro777@gmail.com>
2024-07-01 16:42:07 +0200

    [UPD] created variable flag and recuperated the input value

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // creo un arrey
 const lista = ["marco", "alessandro", "paolo", "vanessa", "cristina"];
 
+// recupero il pulsante dal dom
+let button = document.getElementById('search');
+
+button.addEventListener('click', function(){
+
+
+    // recupero il valore inserito nel campo input
+    let word = document.getElementById('lista').value
+
+
+    // creo una flag o variabile di controllo
+    let found_name = false;
+    
+  
+})
