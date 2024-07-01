9024f577daea5931762b52c23fc78f3c263dbb04
alex2000d <demarcoalessandro777@gmail.com>
2024-07-01 21:41:47 +0200

    [UPD] exercise complete

--- snack 2/script.js
+++ snack 2/script.js
@@ -6,7 +6,14 @@ for(i = 1; i<= 6; i++){
     // chiedo all'utente di inserire il numero sei volte
     let numero = prompt('inserisci il tuo numero');
     
-    
+    // definisco se il numero e pari o dispari
+    if(numero % 2 == 0){
+        console.log`${numero}`;
+    }
+    else{
+        console.log`${numero}`;
+        arrey.push(numero);
+    }
 }
 
 
