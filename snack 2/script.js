b93222d82b6e29808265386af451c864b3fa62a9
alex2000d <demarcoalessandro777@gmail.com>
2024-07-01 21:41:17 +0200

    [UPD] ask the user to insert number for six times

--- snack 2/script.js
+++ snack 2/script.js
@@ -3,7 +3,9 @@ const arrey = [];
 
 // creo ciclo for
 for(i = 1; i<= 6; i++){
-  
+    // chiedo all'utente di inserire il numero sei volte
+    let numero = prompt('inserisci il tuo numero');
+    
     
 }
 
