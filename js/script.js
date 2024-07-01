ec1fa25b512cb2daf657c22f85cea7274db97785
alex2000d <demarcoalessandro777@gmail.com>
2024-07-01 16:43:38 +0200

    cycled rhe arrey and show the result in console

--- js/script.js
+++ js/script.js
@@ -14,5 +14,22 @@ button.addEventListener('click', function(){
     // creo una flag o variabile di controllo
     let found_name = false;
     
-  
+    // ciclo l'arrey e lo confronto con la parola inserita nella form
+    for (let i = 0; i < lista.length; i++){
+        if (lista[i] === word){
+            // se è vera imposto la variabile a true
+            found_name = true;
+        }
+    }
+    // mostro il risultato in console
+
+    if (found_name === true){
+        console.log(`${word} lei è in lista puo entrare`)
+        
+    }
+    else(
+        console.log(`${word} lei non è in lista`)
+       
+       
+    )
 })
