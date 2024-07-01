bdcae22ceb362fba3c82845863b5123546eb8795
alex2000d <demarcoalessandro777@gmail.com>
2024-07-01 22:44:08 +0200

    [UPD] created the arrey

--- snack 3/script.js
+++ snack 3/script.js
@@ -4,6 +4,13 @@ const button = document.getElementById('invia');
 
 // aggiunto una funzione al bottone
 button.addEventListener('click', function(){
-
+    let number = document.getElementById('number').value;
+    console.log(number);
+ // separazione della stringa
+    const separazione = number.split('');
+    console.log(separazione);
+ //    creo un arrey
+      let arrey = [];
+ 
     
  })
